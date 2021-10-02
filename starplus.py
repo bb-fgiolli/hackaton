eaff4138ef2077a593a435483a3173379ede8116
Federico Velasco <81702028+FeFoVelasco@users.noreply.github.com>
2021-10-01 22:58:39 -0300

    starsplus terminado

--- starplus.py
+++ starplus.py
@@ -12,6 +12,7 @@ import math
 import json
 
 main_url = 'https://www.starplus.com/es-419/home'
+cats = [] ###Lista de los contenidos a pasar a un json###
 
 ###Primero iniciamos sesión y elegimos un usuario cualquiera###
 driver = webdriver.Firefox()
@@ -32,15 +33,14 @@ while Y != max_Y:
     time.sleep(8)
     max_Y = driver.execute_script('return scrollMaxY')
 
-###Ahora buscamos los contenidos y nos quedamos con los 10 primeros de cada sección###
+###Ahora buscamos los 10 del main###
 collections = driver.find_elements_by_css_selector("#home-collection > div")
-cats = []
-for collection in collections[2:]:
+for collection in collections[9:]:
     try:
         category_title = collection.find_element_by_tag_name("h4").text
     except:
         print("Esto no es una categoría")
-    cat = {"title":None, "contents":None}
+    cat_main = {"Type":None, "Top":None, "Platform ":None}
     contenidos = collection.find_elements_by_class_name("image-container")
     contenido_titles = []
     n = 0
@@ -49,12 +49,65 @@ for collection in collections[2:]:
         if n > 10:
             continue
         contenido_title = contenido.get_attribute("alt")
-        contenido_titles.append(contenido_title)
-    cat["title"] = category_title
-    cat["contents"] = contenido_titles[:10]
-    cats.append(cat)
-print(cats)
+        contenido_titles.append(str(n) + ". " + contenido_title)
+    cat_main["Type"] = 'Main'
+    cat_main["Top"] = contenido_titles[:10]
+    cat_main["Platform "] = "StarPlus"
+    cats.append(cat_main)
+    print(cat_main)
+    break
 
+###Categoría películas###
+driver.find_element_by_xpath('/html/body/div/div/div[3]/header/nav/ul/span[5]/a/p').click()
+
+collections = driver.find_elements_by_css_selector(".in-view > div")
+for collection in collections:
+    try:
+        category_title = collection.find_element_by_tag_name("h4").text
+    except:
+        print("Esto no es una categoría")
+    cat_mov = {"Type":None, "Top":None, "Platform ":None}
+    contenidos = collection.find_elements_by_class_name("image-container")
+    contenido_titles = []
+    n = 0
+    for contenido in contenidos:
+        n += 1
+        if n > 10:
+            continue
+        contenido_title = contenido.get_attribute("alt")
+        contenido_titles.append(str(n) + ". " + contenido_title)
+    cat_mov["Type"] = 'Movies'
+    cat_mov["Top"] = contenido_titles[:10]
+    cat_mov["Platform "] = "StarPlus"
+    cats.append(cat_mov)
+    print(cat_mov)
+    break
+
+###Categoría series###
+driver.find_element_by_xpath('/html/body/div/div/div[3]/header/nav/ul/span[6]/a/p').click()
+
+collections = driver.find_elements_by_css_selector(".in-view > div")
+for collection in collections:
+    try:
+        category_title = collection.find_element_by_tag_name("h4").text
+    except:
+        print("Esto no es una categoría")
+    cat_ser = {"Type":None, "Top":None, "Platform ":None}
+    contenidos = collection.find_elements_by_class_name("image-container")
+    contenido_titles = []
+    n = 0
+    for contenido in contenidos:
+        n += 1
+        if n > 10:
+            continue
+        contenido_title = contenido.get_attribute("alt")
+        contenido_titles.append(str(n) + ". " + contenido_title)
+    cat_ser["Type"] = 'Series'
+    cat_ser["Top"] = contenido_titles[:10]
+    cat_ser["Platform "] = "StarPlus"
+    cats.append(cat_ser)
+    print(cat_ser)
+    break
 
 with open('starplus.json', 'w') as f:
     json.dump(cats, f)
